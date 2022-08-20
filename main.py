3c7f8353ae3c535115a8c25bf9fc73ee448bfb2a
korocityIssah <issahismail88@gmail.com>
2022-08-20 14:05:13 +0000

    guessing game

---
+++ main.py
@@ -0,0 +1,56 @@
+# importing colors module
+from colors import *
+# importing random module
+from random import randint
+
+# welcome  to Screen
+print(YELLOW, "***** welcome to Lover_Game ******")
+user_input = int(input(WHITE + " 1. Start Game\n "
+                               " 2. About Lover_ Game \n"
+                               " 3. Exit Application\n\n "
+                               "Choose An Option Above : "))
+# determine user input
+if user_input == 1:
+    user_input = int(input(WHITE + "Choose Game Level\n "
+                                   "1. Lazy (1 - 100)\n"
+                                   "2. Intermediate (1 - 500)\n"
+                                   "3. Hard (1 -  1000)\n"
+                                   "provide Level: "))
+    guess_range = 0
+    if user_input == 1:
+        guess_range = 100
+    elif user_input == 2:
+        guess_range = 500
+    elif user_input == 3:
+        guess_range = 1000
+
+    # generate random number base on the guess range
+    generated_number = randint(1, guess_range)
+    # User Guessing Attempt
+    user_chance = 3
+    for chance in range(3):
+        user_guess = int(input(WHITE+ "Provide Your Guess Number : "))
+        # Determine / Validate User Guess
+        if user_guess == generated_number:
+            print(GREEN, f"Congratulation!!! You Guess {generated_number} ")
+            break
+        elif user_guess > generated_number:
+            print(YELLOW, f" {user_guess} Too High, Try Again!!!")
+            user_chance -= 1
+        elif user_guess < generated_number:
+            print(YELLOW, f"Too Low, {user_guess} Try Again!!!")
+            user_chance -= 1
+
+            if user_chance < 1:
+                print(RED, f"You Failed, Try Again!!!!!\n"
+                           f"The Correct Guess Number is {generated_number}")
+
+
+elif user_input == 2:
+    print(YELLOW, "Loving Guessing Numbers")
+elif user_input == 3:
+    print(WHITE, " Hope To see You Again Gamer. ")
+    exit(1)
+else:
+    pass
+print(RED, "Invalid Option, Try Again!!!!")
